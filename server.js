8ef162cc6d190bc3a4d82cdc1834682a2d258618
Javier Vergara <ja.vergarac96@gmail.com>
2024-05-25 04:32:22 -0400

    server.js

--- server.js
+++ server.js
@@ -57,9 +57,6 @@ app.get("/", async (req, res) => {
     }
 });
 
-let status = "";
-let message = "";
-
 app.get("/registro", (req, res) => {
     res.render("Registro");
 });
@@ -112,6 +109,20 @@ app.get("/Admin", async (req, res) => {
     };
 });
 
+//API REST de Skaters
+
+app.get("/skaters", async (req, res) => {
+    const skaters = await getSkaters();
+    try {
+        res.status(200).send(skaters);
+    } catch (e) {
+        res.status(500).send({
+            error: `Algo salio mal... ${e}`,
+            code: 500
+        })
+    }
+});
+
 app.post("/skaters", async (req, res) => {
     const { email, nombre, password, anos_experiencia, especialidad} = req.body;
     const { files } = req;
@@ -134,180 +145,61 @@ app.post("/skaters", async (req, res) => {
     })
 })
 
-// app.post('/registro', async (req, res) => {
-//     const { email, nombre, password, passwordConfirm, anos_experiencia, especialidad, foto } = req.params;
+app.put("/skaters", async (req, res) => {
+    const {id, nombre, anos_experiencia, especialidad} = req.body;
+    try {
+        const skaterB = await querys.consultaSkater(id);
+        console.log("SKATER", skaterB);
+        if (skaterB) {
+            const query = await querys.editar(id, nombre, anos_experiencia, especialidad);
+            res.status(200).send("Datos actualizados con exito");
+        } else {
+            res.status(400).send("No existe este Skater");
+        }
 
-//     const fotoPath = `${__dirname}/public/${nombre}.jpg`;
+    } catch (e) {
+        res.status(500).send({
+            error: `Algo salio mal... ${e}`,
+            code:500
+        })
+    };
+});
 
-//     try {
-//         if (email && nombre && password && passwordConfirm && anos_experiencia && especialidad && foto) {
-//             if (password === passwordConfirm) {
-//                 if (!req.files || !req.files.target_file) {
-//                     return res.status(400).send("Por favor, ingresa una imagen para subir");
-//                 }
+app.put("/skaters/status/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const skaterB = await querys.consultaSkater(id);
+        if (skaterB) {
+            const query = await querys.aprobar(id);
+            res.status(200).send("Estado Actualizado con exito");
+        } else {
+            res.status(400).send("No existe este Skater");
+        }
+    } catch (e) {
+        res.status(500).send({
+            error: `Algo salio mal... ${e}`,
+            code: 500
+        })
+    };
+});
 
-//                 const { target_file } = req.files;
-
-//                 target_file.mv(fotoPath, (err) => {
-//                     if (err) res.status(500).send(err)
-//                     foto = fotoPath;
-//                 })
-
-//                 const query = await querys.registrar(email, nombre, password, anos_experiencia, especialidad, foto);
-
-//                 res.status(201).json({
-//                     message: 'Skater registrado correctamente.',
-//                     skater: query.rows
-//                 })
-
-//             } else {
-//                 console.log("La contraseña ingresada deben ser la misma.");
-//                 return res.status(400).json({ error: 'La contraseña ingresada deben ser la misma.' });
-//             }
-
-//         } else {
-//             console.log("Por favor, proporciona todos los datos para continuar.");
-//             return res.status(400).json({ error: 'Por favor, proporciona todos los datos para continuar.' });
-//         }
-//     } catch (err) {
-//         console.log("Error General: ", err)
-//         const final = errors(err.code, status, message);
-//         console.log("Codigo de Error: ", final.code);
-//         console.log("Status de Error: ", final.status);
-//         console.log("Mensaje de Error: ", final.message);
-//         console.log("Error Original: ", err.message);
-//         res.status(final.status).json(final.message);
-//     }
-// })
-
-// app.get('/login', (req, res) => {
-//     const { email, password } = req.params;
-
-//     const skater = querys.login(email, password);
-
-//     const skaterID = querys.consultaSkater(email);
-
-//     try {
-
-//         if (skater.find((s) => s.email === email && s.password === password)) {
-//             const token = jwt.sign({
-//                 exp: Math.floor(Date.now() / 1000) + 120,
-//                 data: skater,
-//                 id: skaterID
-//             }, secretKey);
-
-//             res.redirect(`/restricted?token=${token}`);
-//         } else {
-//             res.status(401).send("Usuario o contraseña incorrecta");
-//         }
-
-//     } catch (err) {
-//         console.log("Error General: ", err)
-//         const final = errors(err.code, status, message);
-//         console.log("Codigo de Error: ", final.code);
-//         console.log("Status de Error: ", final.status);
-//         console.log("Mensaje de Error: ", final.message);
-//         console.log("Error Original: ", err.message);
-//         res.status(final.status).json(final.message);
-//     }
-// })
-
-// const verifyToken = (req, res, next) => {
-//     const token = req.query.token || req.headers['x-access-token'];
-
-//     if (!token) {
-//         return res.status(401).send("Acceso no autorizado, token es requerido");
-//     }
-
-//     jwt.verify(token, secretKey, (err, user) => {
-//         if (err) {
-//             return res.status(403).send("Token inválido o expirado");
-//         }
-//         req.user = user;
-//         next();
-//     });
-// };
-
-// app.get('/restricted', verifyToken, (req, res) => {
-
-// })
-
-// app.put('/editar', async (req, res) => {
-//     const { email, nombre, password, passwordConfirm, anos_experiencia, especialidad } = req.params;
-
-//     try {
-//         if (email && nombre && password && passwordConfirm && anos_experiencia && especialidad) {
-//             if (password === passwordConfirm) {
-
-//                 await querys.editar(email, nombre, password, anos_experiencia, especialidad);
-
-//                 const skaterActualizado = await querys.consultaSkater(email);
-
-//                 if (skaterActualizado.length > 0) {
-//                     res.json({
-//                         message: 'Skater actualizado correctamente',
-//                         skater: skaterActualizado[0]
-//                     });
-//                 }
-
-//             } else {
-//                 console.log("La contraseña ingresada deben ser la misma.");
-//                 return res.status(400).json({ error: 'La contraseña ingresada deben ser la misma.' });
-//             }
-//         } else {
-//             console.log("Por favor, proporciona todos los datos para continuar.");
-//             return res.status(400).json({ error: 'Por favor, proporciona todos los datos para continuar.' });
-//         }
-//     } catch (err) {
-//         console.log("Error General: ", err)
-//         const final = errors(err.code, status, message);
-//         console.log("Codigo de Error: ", final.code);
-//         console.log("Status de Error: ", final.status);
-//         console.log("Mensaje de Error: ", final.message);
-//         console.log("Error Original: ", err.message);
-//         res.status(final.status).json(final.message);
-//     }
-// })
-
-// app.delete('/eliminar', async (req, res) => {
-//     const { email } = req.params;
-//     try {
-//         const query = await querys.eliminar(email);
-
-//         res.json({
-//             message: 'Usuario eliminado correctamente'
-//         });
-
-//     } catch (err) {
-//         console.log("Error General: ", err)
-//         const final = errors(err.code, status, message);
-//         console.log("Codigo de Error: ", final.code);
-//         console.log("Status de Error: ", final.status);
-//         console.log("Mensaje de Error: ", final.message);
-//         console.log("Error Original: ", err.message);
-//         res.status(final.status).json(final.message);
-//     }
-// })
-
-// app.put('/aprobar', async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const query = await querys.aprobar(id);
-
-//         res.json({
-//             message: 'Skater aprobado correctamente'
-//         });
-
-//     } catch (err) {
-//         console.log("Error General: ", err)
-//         const final = errors(err.code, status, message);
-//         console.log("Codigo de Error: ", final.code);
-//         console.log("Status de Error: ", final.status);
-//         console.log("Mensaje de Error: ", final.message);
-//         console.log("Error Original: ", err.message);
-//         res.status(final.status).json(final.message);
-//     }
-// })
+app.delete("/skaters/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const skaterB = await querys.consultaSkater(id);
+        if (skaterB) {
+            const query = await querys.eliminar(id);
+            res.status(200).send("Skater Eliminado con éxito");
+        } else {
+            res.status(400).send("No existe este Skater");
+        }
+    } catch (e) {
+        res.status(500).send({
+            error: `Algo salió mal... ${e}`,
+            code: 500
+        })
+    };
+});
 
 app.use((req, res) => {
     res.send('Esta página no existe...');
